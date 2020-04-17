840042834ab2419657148f0df35b045c26fd9b6e
绝尘 <17373173061@163.com>
2020-04-17 10:10:17 +0800

    科室模块

---
+++ src/views/modules/department/Department-add-or-update.vue
@@ -0,0 +1,82 @@
+<template>
+  <el-dialog
+    :title="!dataForm.id ? '新增' : '修改'"
+    :close-on-click-modal="false"
+    :visible.sync="visible">
+    <el-form :model="dataForm" :rules="dataRule" ref="dataForm" @keyup.enter.native="dataFormSubmit()" label-width="120px">
+      <el-form-item label="" prop="departmentName">
+        <el-input v-model="dataForm.departmentName" placeholder=""></el-input>
+      </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="visible = false">取消</el-button>
+      <el-button type="primary" @click="dataFormSubmit()" :disabled="confirmButtonDisabled">确定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      visible: false,
+      confirmButtonDisabled: false,
+      dataForm: {},
+      dataRule: {
+        departmentId: [{ required: true, message: '不能为空', trigger: 'blur' }],
+        departmentName: [{ required: true, message: '不能为空', trigger: 'blur' }]
+      }
+    }
+  },
+  methods: {
+    init (id) {
+      this.dataForm.id = id || ''
+      this.visible = true
+      this.confirmButtonDisabled = false
+      this.$nextTick(() => {
+        this.$refs['dataForm'].resetFields()
+        if (this.dataForm.id) {
+          this.$http({
+            url: this.$http.adornUrl(`/department/Department/info/${this.dataForm.id}`),
+            method: 'get',
+            params: this.$http.adornParams()
+          }).then(({data}) => {
+            if (data && data.code === 200) {
+              this.dataForm = data.Department
+            }
+          })
+        } else {
+          this.dataForm = {}
+        }
+      })
+    },
+    // 表单提交
+    dataFormSubmit () {
+      this.$refs['dataForm'].validate((valid) => {
+        if (valid) {
+          this.$http({
+            url: this.$http.adornUrl(`/department/Department/${!this.dataForm.id ? 'save' : 'update'}`),
+            method: !this.dataForm.id ? 'post' : 'put',
+            data: this.$http.adornData(this.dataForm)
+          }).then(({data}) => {
+            this.confirmButtonDisabled = true
+            if (data && data.code === 200) {
+              this.$message({
+                message: '操作成功',
+                type: 'success',
+                duration: 1000,
+                onClose: () => {
+                  this.visible = false
+                  this.$emit('refreshDataList')
+                }
+              })
+            } else {
+              this.$message.error(data.msg)
+            }
+          })
+        }
+      })
+    }
+  }
+}
+</script>
