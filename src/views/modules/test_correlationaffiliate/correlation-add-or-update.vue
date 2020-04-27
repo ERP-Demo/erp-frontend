fcb8c6f6550158cf43b4a80b455abcd179c6dfd1
Haoran Xia <1042122451@qq.com>
2020-04-27 15:53:01 +0800

    化验项目

--- src/views/modules/test_correlationaffiliate/correlation-add-or-update.vue
+++ src/views/modules/test_correlationaffiliate/correlation-add-or-update.vue
@@ -103,6 +103,43 @@ export default {
           })
         }
       })
+    },
+    // 获取数据列表
+    getDataList () {
+      this.dataListLoading = true
+      this.$http({
+        url: this.$http.adornUrl('/test_correlationaffiliate/correlation/listwindows'),
+        method: 'get',
+        params: this.$http.adornParams({
+          'page': this.pageIndex,
+          'limit': this.pageSize,
+          'key': this.dataForm.key
+        })
+      }).then(({data}) => {
+        if (data && data.code === 200) {
+          this.dataList = data.page.list
+          this.totalPage = data.page.totalCount
+        } else {
+          this.dataList = []
+          this.totalPage = 0
+        }
+        this.dataListLoading = false
+      })
+    },
+    // 每页数
+    sizeChangeHandle (val) {
+      this.pageSize = val
+      this.pageIndex = 1
+      this.getDataList()
+    },
+    //当前页
+    currentChangeHandle (val) {
+      this.pageIndex = val
+      this.getDataList()
+    },
+    // 多选
+    selectionChangeHandle (val) {
+      this.dataListSelections = val
     }
   }
 }
