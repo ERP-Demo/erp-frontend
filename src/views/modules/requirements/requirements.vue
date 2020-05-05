43f523548f4b3e75bacd878a04880243fe67b2b2
dome <81118597@qq.com>
2020-05-05 13:57:25 +0800

    供货商药库关联模块更新

--- src/views/modules/requirements/requirements.vue
+++ src/views/modules/requirements/requirements.vue
@@ -6,7 +6,7 @@
       </el-form-item>
       <el-form-item>
         <el-button @click="getDataList()">查询</el-button>
-        <el-button v-if="isAuth('requirements:requirements:save')" type="primary" @click="addOrUpdateHandle()">新增</el-button>
+<!--        <el-button v-if="isAuth('requirements:requirements:save')" type="primary" @click="addOrUpdateHandle()">新增</el-button>-->
         <el-button v-if="isAuth('requirements:requirements:delete')" type="danger" @click="deleteHandle()" :disabled="dataListSelections.length <= 0">批量删除</el-button>
       </el-form-item>
     </el-form>
@@ -71,7 +71,7 @@
         width="150"
         label="操作">
         <template slot-scope="scope">
-          <el-button type="text" size="small" @click="addOrUpdateHandle(scope.row.id)">修改</el-button>
+<!--          <el-button type="text" size="small" @click="addOrUpdateHandle(scope.row.id)">修改</el-button>-->
           <el-button type="text" size="small" @click="deleteHandle(scope.row.id)">删除</el-button>
         </template>
       </el-table-column>
