9adebe06b663904245de4d52bb5d6ced7f6a2602
41369ly <41333369@qq.com>
2020-05-17 22:19:19 +0800

    前端页面bug修复

--- src/views/modules/outpatient/maintain/inspection.vue
+++ src/views/modules/outpatient/maintain/inspection.vue
@@ -141,10 +141,8 @@
     </div>
 </template>
 <script>
-    import {getNondrugModelList, updateModel, createModel, deleteModel} from '@/api/nondrugmodel'
-    import {getAllNondrug} from '@/api/non_drug'
     import Pagination from '@/components/Pagination'
-    import {deepClone, parseTime} from '@/utils'
+    import {parseTime} from '@/utils'
 
     export default {
         components: {Pagination},
